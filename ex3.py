1bf60aceee5b1384800048b4114cc353e52627eb
nguyennam9696 <nguyennam9696@gmail.com>
2015-09-05 16:58:27 -0700

    Learn print, variables, numbers, strings

---
+++ ex3.py
@@ -0,0 +1,14 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_available = cars - drivers
+carpool = drivers * space_in_a_car
+average_passengers_per_car = passengers / drivers
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_available, "cars available."
+print carpool, "passengers to carpool available."
+print carpool, "passengers to carpool available."
+print carpool, "passengers to carpool available."
---
+++ ex4.py
@@ -0,0 +1,5 @@
+name = 'Nam Nguyen'
+age = 24
+height = 5.8
+weight = 135
+print "If I add %d age, %d height, and %d weight altogether, I would get %d" % (age, height, weight, age + height + weight)
