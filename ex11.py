effba5250d92f556ae7411915b3688d51cfae797
nguyennam9696 <nguyennam9696@gmail.com>
2015-09-05 18:17:54 -0700

    Second method of getting raw input from user

---
+++ ex11.py
@@ -0,0 +1,5 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So you are %s years old, %s feet tall, and %s pounds" % (age, height, weight)
