9ffb384f17778791b1c539c393ea50632bd6d5b7
nguyennam9696 <nguyennam9696@gmail.com>
2015-09-05 18:05:05 -0700

    Getting User Input

---
+++ ex10.py
@@ -0,0 +1,7 @@
+print "How tall are you?",
+height = raw_input()
+print "How old are you?",
+age = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+print "So you are %s years old, %s feet tall, and %s pounds" % (age, height, weight)
